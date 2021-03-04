d32a1e2bf2ea5f27e699a884d129cca967b44fa5
温华鹏 <shiyue32123@126.com>
2021-03-04 21:26:56 +0800

    提交结构体部分

--- github.com/daily_study/11_gouzaofangfa/main.go
+++ github.com/daily_study/11_gouzaofangfa/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 // 方法和接收者：go中的方法是一种作用于特定类型变量的函数。这种特定类型叫做接收者
 // 语法：func(接收者变量，接收者类型) 方法名(参数列表) (返回参数){ 函数体 }
@@ -9,25 +12,207 @@ import "fmt"
 // 防范名、参数列表、返回参数；
 
 // 1.举例
-type Person struct {
-	name string
-	age int8
+//type Person struct {
+//	name string
+//	age int8
+//}
+//// 1.1.构造函数
+//func NewPerson(name string, age int8) *Person{
+//	return &Person{
+//		name: name,
+//		age: age,
+//	}
+//} // 1.2.Dream Person做梦的方法 func (p Person) Dream() {
+//	fmt.Printf("%s的梦想是学好Go语言！\n", p.name)
+//}
+//func main() {
+//	p1 := NewPerson("小王子", 18)
+//	p1.Dream()
+//}
+
+// 1.1.指针类型的接收者：指针类型的接收者由一个结构体的指针组成，由于指针的特性，调用方法时修改接收者指针的任意成员变量，在方法结束后都是有效的。
+//func (p *Person) SetAge(newAge int8){
+//	p.age = newAge
+//}
+//
+//func main() {
+//	p1 := NewPerson("小王子", 18)
+//	p1.SetAge(28)
+//	fmt.Println(p1.age)
+//}
+
+// 1.2.值类型的接收值：当方法作用于值类型接收者时，go语言会在代码运行时将接收者的值复制一份。在值类型接收者的方法中可以获取接收者的成员值，但修改
+// 操作知识针对副本，无法修改接收者变量本身
+//func (p Person) SetAge2(newAge int8) {
+//	p.age = newAge
+//}
+//
+//func main() {
+//	p1 := NewPerson("小王子", 18)
+//	p1.SetAge2(28)
+//	fmt.Println(p1.age)
+//}
+
+// 1.3.总结：什么时候用指针类型接收者
+// 		1.需要修改接收者中的值
+// 		2.接收者时拷贝代价比较大的大对象
+//   	3.保证一致性，如果某个方法使用了指针接收者，那么其他的方法也应该使用指针接收者
+
+
+// 2.任意类型添加方法：接收者的类型可以是任何类型，不仅仅是结构体，任何类型都可以拥有方法。不能给别的包的类型定义方法
+//type MyInt int
+//
+//func (i MyInt) SayHello(){
+//	fmt.Println("Hello, I am a Int")
+//}
+//
+//func main() {
+//	var v1 MyInt
+//	v1.SayHello()
+//	v1 = 100
+//	fmt.Printf("%#v %T\n", v1, v1)
+//}
+
+// 3.结构体的匿名字段：结构体允许其成员字段在声明时没有字段名而只有类型，这种没有名字的字段就是匿名字段
+// 匿名字段的说法并不是说没有字段名，而是会采用类型名来做为字段名，结构体要求字段名称必须唯一，因为一个结构体中同种类型的匿名字段只有一个
+//type Person struct{
+//	string
+//	int8
+//}
+//
+//func main() {
+//	p1 := Person{"小王子", 18}
+//	fmt.Printf("%#v\n", p1)
+//	fmt.Println(p1.string, p1.int8)
+//}
+
+// 4.嵌套结构体：一个结构体中可以嵌套的包含另一个结构体或者结构体指针；这个结构体中用到了匿名字段
+//type Address struct {
+//	Province, City string
+//}
+//type User struct {
+//	Name, Gender string
+//	Address
+//}
+//
+//func main() {
+//	user1 := User{
+//		"小王子",
+//		"男",
+//		Address{
+//			"河北",
+//			"衡水",
+//		},
+//	}
+//	fmt.Printf("%#v \n", user1)
+//}
+
+// 4.1.嵌套结构体中可能存在相同的字段名，为了避免歧义需要指定具体的内嵌套结构体字段
+//type Address struct {
+//	Province, City, CreateTime string
+//}
+//
+//type Email struct {
+//	Account, CreateTime string
+//}
+//
+//type User struct {
+//	Name, Gender string
+//	Address
+//	Email
+//}
+//
+//func main() {
+//	var user User
+//	user.Name = "小王子"
+//	user.Gender = "男"
+//	user.Address = Address{
+//		"河北",
+//		"衡水",
+//		"2021-03-04",
+//	}
+//	user.Email = Email{
+//		"xiaowangzi@126.com",
+//		"2023-03-04",
+//	}
+//	fmt.Printf("%#v \n", user)
+//	fmt.Println(user.Address.CreateTime, user.Email.CreateTime)
+//}
+
+// 5.结构体的继承：结构体可以像其他语言中面向对象的集成
+//type Animal struct {
+//	name string
+//}
+//
+//func (a *Animal)move(){
+//	fmt.Printf("%s会动\n", a.name)
+//}
+//
+//type Dog struct {
+//	Feet int8
+//	*Animal  // 通过嵌套匿名结构体实现继承
+//}
+//
+//func (d *Dog) wang() {
+//	fmt.Printf("%s会汪汪汪~\n", d.name)
+//}
+//
+//func main() {
+//	d1 := &Dog{
+//		Feet: 4,
+//		Animal: &Animal{
+//			name: "大黄",
+//		},
+//	}
+//	d1.wang()
+//	d1.move()
+//}
+
+// 6.结构体字段的可见性：结构体中字段大写开头表示可公开访问，小写表示私有
+
+// 7.结构体和json序列化
+type Student struct {
+	Id int
+	Gender string
+	Name string
 }
 
-// 1.1.构造函数
-func NewPerson(name string, age int8) *Person{
-	return &Person{
-		name: name,
-		age: age,
-	}
-}
-
-// 1.2.Dream Person做梦的方法
-func (p Person) Dream() {
-	fmt.Printf("%s的梦想是学好Go语言！\n", p.name)
+type Class struct {
+	Title string
+	Students []*Student
 }
 
 func main() {
-	p1 := NewPerson("小王子", 18)
-	p1.Dream()
+	c := &Class{
+		Title: "火箭101",
+		Students: make([]*Student, 0, 30),
+	}
+
+	for i:=1; i < 10; i++ {
+		stu := &Student{
+			Name: fmt.Sprintf("stu%02d", i),
+			Gender: "男",
+			Id: i,
+		}
+		c.Students = append(c.Students, stu)
+	}
+
+	// json序列化, 结构体-->json格式的字符串
+	data, err := json.Marshal(c)
+	if err != nil{
+		fmt.Println("json marshal filed")
+		return
+	}
+	fmt.Printf("json:%s\n", data)
+
+	// 反序列化
+	str := `{"Title":"101","Students":[{"ID":0,"Gender":"男","Name":"stu00"},{"ID":1,"Gender":"男","Name":"stu01"},{"ID":2,"Gender":"男","Name":"stu02"},{"ID":3,"Gender":"男","Name":"stu03"},{"ID":4,"Gender":"男","Name":"stu04"},{"ID":5,"Gender":"男","Name":"stu05"},{"ID":6,"Gender":"男","Name":"stu06"},{"ID":7,"Gender":"男","Name":"stu07"},{"ID":8,"Gender":"男","Name":"stu08"},{"ID":9,"Gender":"男","Name":"stu09"}]}`
+	c1 := &Class{}
+	err = json.Unmarshal([]byte(str), c1)
+	if err != nil {
+		fmt.Println("json unmarshal failed!")
+		return
+	}
+	fmt.Printf("%#v\n", c1)
 }
+
