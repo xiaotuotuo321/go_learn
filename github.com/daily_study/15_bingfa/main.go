3cc1b678a6284041decc0ff548034763d64bf416
温华鹏 <shiyue32123@126.com>
2021-03-20 10:21:46 +0800

    添加并发部分学习

--- github.com/daily_study/15_bingfa/main.go
+++ github.com/daily_study/15_bingfa/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"sync"
 )
 
@@ -477,17 +478,145 @@ Wait()						阻塞到计数器变为0
 // sync.WaitGroup内部维护着一个计数器，计数器的值可以增加和减少。当启动了N个并发任务时，就将计数器值增加N。每个任务完成时通过调用Done()方法将
 // 计数器减1，通过调用Wait()来等待并发任务执行完，当计数器值为0时，表示所有的并发任务已经完成
 
-var wg sync.WaitGroup
+//var wg sync.WaitGroup
+//
+//func hello() {
+//	defer wg.Done()
+//	fmt.Println("Hello Goroutine")
+//}
+//
+//func main() {
+//	wg.Add(1)
+//	go hello()	// 启动另外一个goroutine去执行hello函数
+//	fmt.Println("main goroutine done!")
+//	wg.Wait()
+//}
+// sync.WaitGroup是一个结构体，传递的时候要传指针
 
-func hello() {
-	defer wg.Done()
-	fmt.Println("Hello Goroutine")
-}
+// 7.4. sync.Once 这是一个进阶知识点
+// 在编程的很多场景下我们需要确保某些操作在高并发的场景下只执行一次，比如只加载一次配置文件、只关闭一次通道等。
+// go预言者中sync包中提供了一个针对只执行一次场景的解决方案- sync.Once 它只有一个Do方法
+// func (o *Once) Do(f func()) {} 如果要执行的函数f 需要传递参数就需要搭配闭包来使用
+
+// 加载配置文件示例
+/*
+延迟一个开销很大的初始化操作到真正用到它的时候再执行是一个很好的实践，因为预先初始化一个变量（比如在init函数中完成初始化）会增加程序的启动耗时，而且
+有可能实际执行过程中这个变量没有用上，那么这个初始化操作就不是必须要做的
+*/
+
+// 7.4.1.
+//var icons map[string]image.Image
+//
+//func loadIcons() {
+//	icons = map[string]image.Image{
+//		"left": loadIcon("left.png"),
+//		"up": loadIcon("up.png"),
+//		"right": loadIcon("right.png"),
+//		"down": loadIcon("down.png"),
+//	}
+//}
+//
+//// Icon 被多个goroutine调用时并不是并发安全的
+//func Icon(name string) image.Image {
+//	if icons == nil{
+//		loadIcons()
+//	}
+//	return icons[name]
+//}
+
+// 7.4.2.
+// 对个goroutine并发调用Icon函数时不是并发安全的，现代的编译器和CPU可能会在保证每个goroutine都满足串行一致的基础上自由地重排访问内存的顺序。
+// loadIcons函数可能会被重排为以下结果
+
+//func loadIcons() {
+//	icons = make(map[string]image.Image)
+//	icons["left"] = loadIcon("left.png")
+//	icons["up"] = loadIcon("up.png")
+//	icons["right"] = loadIcon("right.png")
+//	icons["down"] = loadIcon("down.png")
+//}
+
+//在这种情况下就会出现判断了icons不是nil也不意味着变量初始化完成了。考虑到这种情况，我们能想到的办法是添加互斥锁，保证初始化icons的时候不会被
+// 的goroutine操作，但是这样做优惠引发性能问题
+
+// 使用sync.Once改造的代码如下
+//var icons map[string]image.Image
+//
+//var loadIconsOnce sync.Once
+//
+//func loadIcons() {
+//	icons = map[string]image.Image{
+//		"left": loadIcon("left.png"),
+//		"up": loadIcon("up.png"),
+//		"right": loadIcon("right.png"),
+//		"down": loadIcon("down.png"),
+//	}
+//}
+//// Icon 是并发安全的
+//func Icon(name string) image.Image{
+//	loadIconsOnce.Do(loadIcons)
+//	return icons[name]
+//}
+
+// 7.4.3.并发安全的单例模式：借助sync.Once实现的并发安全的单例模式
+//type singleton struct {}
+//
+//var instance *singleton
+//var once sync.Once
+//
+//func GetInstance() *singleton {
+//	once.Do(func() {
+//		instance = &singleton{}
+//	})
+//	return instance
+//}
+
+// sync.Once其实内部包含一个互斥锁和一个布尔值，互斥锁保证布尔值和数据的安全，而布尔值来记录初始化是否完成。这样设计就能保证初始化操作的时候是并发
+// 安全的并且初始化操作也不会被执行多次
+
+// 7.4.4.sync.Map: go语言中内置的map不是并发安全的
+//var m = make(map[string]int)
+//
+//func get(key string) int{
+//	return m[key]
+//}
+//
+//func set(key string, value int){
+//	m[key] = value
+//}
+//
+//func main() {
+//	wg := sync.WaitGroup{}
+//	for i := 0; i < 2; i++{
+//		wg.Add(1)
+//		go func(n int) {
+//			key := strconv.Itoa(n)
+//			set(key, n)
+//			fmt.Printf("k=%v, v=%v\n", key, get(key))
+//			wg.Done()
+//		}(i)
+//	}
+//	wg.Wait()
+//}
+
+// 上面的代码开启少量几个goroutine的时候可能没有什么问题，当并发对了之后执行上面的代码就会出现fatal error: concurrent map writes 错误
+
+// 像这种场景下就需要为map加锁来保证并发的安全性了，go语言的sync包中提供了一个开箱即用的并发安全版map-sync.Map。开箱即用表示不用像内置的map一样使用
+// make函数初始化就能直接使用。同时sync.map内置了诸如Store、Load、LoadOrStore、Delete、Range等操作方法。
+
+var m = sync.Map{}
 
 func main() {
-	wg.Add(1)
-	go hello()	// 启动另外一个goroutine去执行hello函数
-	fmt.Println("main goroutine done!")
+	wg := sync.WaitGroup{}
+	for i := 0; i < 20; i++{
+		wg.Add(1)
+		go func(n int) {
+			key := strconv.Itoa(n)
+			m.Store(key, n)
+			value, _ := m.Load(key)
+			fmt.Printf("k=%v, v=%v\n", key, value)
+			wg.Done()
+		}(i)
+	}
 	wg.Wait()
 }
-// sync.WaitGroup是一个结构体，传递的时候要传指针
