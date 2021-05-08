e7115530a74a562b6f4eb41c786eb0459cdc257b
温华鹏 <shiyue32123@126.com>
2021-05-08 17:53:13 +0800

    添加第四天分组控制学习

---
+++ github.com/seven_day/for_Gee/day4/gee/gee.go
@@ -0,0 +1,64 @@
+package gee
+
+import (
+	"log"
+	"net/http"
+)
+
+type HandlerFunc func(*Context)
+
+type RouterGroup struct{
+	prefix string
+	middlewares []HandlerFunc
+	parent *RouterGroup
+	engine *Engine
+}
+
+type Engine struct{
+	*RouterGroup
+	router *router
+	groups []*RouterGroup
+}
+
+func New() *Engine {
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+
+	return engine
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc){
+	pattern := group.prefix + comp
+
+	log.Printf("Route %4s - %s", method, comp)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+func (group *RouterGroup) GET (pattern string, handler HandlerFunc){
+	group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST (pattern string, handler HandlerFunc){
+	group.addRoute("POST", pattern, handler)
+}
+
+func (e *Engine) ServeHTTP (w http.ResponseWriter, req *http.Request){
+	c := NewContext(w, req)
+	e.router.handle(c)
+}
+
+func (e *Engine) Run(addr string) (err error){
+	return http.ListenAndServe(addr, e)
+}
---
+++ github.com/seven_day/for_Gee/day4/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"go_learn/github.com/seven_day/for_Gee/day4/gee"
+	"net/http"
+)
+
+// 第四天：主要是分组控制；
+
+func main() {
+	r := gee.New()
+
+	r.GET("/index", func(c *gee.Context){
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	})
+
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
+
+		v1.GET("/hello", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
+
+	r.Run(":9999")
+}
