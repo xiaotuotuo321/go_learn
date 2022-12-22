492194f055af9a93237b8f0e39f4d84d25044396
huapeng.wen <huapeng.wen@ihandysoft.com>
2022-12-22 16:42:53 +0800

    更改返回status_code

--- github.com/2.seven_day/1.for_Gee/day2/gee/router.go
+++ github.com/2.seven_day/1.for_Gee/day2/gee/router.go
@@ -1,6 +1,7 @@
 package gee
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -13,17 +14,17 @@ func newRouter() *router {
 	return &router{handlers: make(map[string]HandlerFunc)}
 }
 
-func (r *router) addRoute(method string, pattern string, handler HandlerFunc){
+func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
 	log.Printf("Route %4s - %s", method, pattern)
 	key := method + "-" + pattern
 	r.handlers[key] = handler
 }
 
-func (r *router) handle(c *Context){
+func (r *router) handle(c *Context) {
 	key := c.Method + "-" + c.Path
-	if handler, ok := r.handlers[key]; ok{
+	if handler, ok := r.handlers[key]; ok {
 		handler(c)
 	} else {
-		c.String(http.StatusNotModified, "404 NOT FOUND: %s\n", c.Path)
+		c.String(http.StatusNotFound, fmt.Sprintf("404 NOT FOUND: %s\n", c.Path))
 	}
 }
